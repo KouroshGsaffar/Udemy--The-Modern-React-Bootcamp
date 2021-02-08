3f1f0f8f0f432a28355324cbb32b9a0d0698d87f
KouroshGsaffar <kooroshgharooni74@gmail.com>
2021-02-08 17:08:16 -0500

    App structure

---
+++ 2. Introducing JSX/App Structure/NumPicker.js
@@ -0,0 +1,25 @@
+function getNum(){
+    return Math.floor(Math.random()*10) + 1;
+}
+
+class NumPicker extends React.Component {
+    render() {
+        const num =  getNum()
+        let msg;
+        if (num ===7){
+            msg=
+            <div>
+                <h2>CONGRATS YOU WIN!</h2>
+                <img src='https://media4.giphy.com/media/Wwe6DNWWOvfBbr9Gbc/source.gif' />
+            </div>
+        }else{
+            msg='SORRY YOU LOSE!'
+        }
+        return(
+            <div>
+                <h1>Your number is: {num} </h1>
+                {msg}
+            </div>
+        )
+    }
+}
--- 2. Introducing JSX/index.js
+++ 2. Introducing JSX/index.js
@@ -6,8 +6,8 @@ class JSXDemo extends React.Component{
     render(){
         return(
             <div>
-                {/* <h1>My Image!</h1>
-                <img src="https://images.unsplash.com/photo-1529978755210-7f13333beb13?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" /> */}
+                <h1>My Image!</h1>
+                <img src="https://images.unsplash.com/photo-1529978755210-7f13333beb13?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" />
                 <h1>My Current Mood is: {getMood()}</h1>
             </div>
         );
