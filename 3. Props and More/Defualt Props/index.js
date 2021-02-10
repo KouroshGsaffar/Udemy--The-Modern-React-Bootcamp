a1c096cbf6a18f507d1b2098b713d6fa8858270b
KouroshGsaffar <kooroshgharooni74@gmail.com>
2021-02-09 19:48:55 -0500

    Props

---
+++ 3. Props and More/Defualt Props/index.js
@@ -0,0 +1,14 @@
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <Hello to="Ringo" from="Paul" bangs={3}
+                />
+                <Hello to="George"
+                />
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'))
---
+++ 3. Props and More/Props and More Slot Machine/Machine.js
@@ -0,0 +1,17 @@
+class Machine extends React.Component{
+    render(){
+        let msg;
+        const {s1,s2,s3}=this.props;
+        if((s1===s2) && (s1===s3)){
+            msg="You win!"
+        }else{
+            msg="You lose!"
+        }
+        return(
+            <div>
+                <p>{s1} {s2} {s3}</p>
+                {msg}
+            </div>
+        )
+    }
+}
