ded5c37390a9c0d8f50caa99c5e318b30d6ecf25
KouroshGsaffar <kooroshgharooni74@gmail.com>
2021-02-18 22:13:36 -0500

    lightOutGame

--- 12. Light Out Game/react-lights-out-starter/src/Board.js
+++ 12. Light Out Game/react-lights-out-starter/src/Board.js
@@ -74,19 +74,28 @@ class Board extends Component {
         board[y][x] = !board[y][x];
       }
     }
-
+    flipCell(y,x);
+    flipCell(y,x-1);
+    flipCell(y,x+1);
+    flipCell(y-1,x);
+    flipCell(y+1,x);
     // TODO: flip this cell and the cells around it
 
     // win when every cell is turned off
     // TODO: determine is the game has been won
+    let hasWon=board.every(row => row.every(cell => !cell));
 
-    // this.setState({board, hasWon});
+    this.setState({board:board, hasWon:hasWon});
   }
 
 
   /** Render game board or winning message. */
 
   render() {
+    if(this.state.hasWon){
+      return <h1>You Won!!!</h1>
+    }
+    
 
     // if the game is won, just show a winning msg & render nothing else
 
@@ -100,7 +109,7 @@ class Board extends Component {
       let row=[];
       for(let x=0; x<this.props.ncols;x++){
         let coord = `${y}-${x}`
-        row.push(<Cell isLit={this.state.board[y][x]} key={coord} /> )
+        row.push(<Cell isLit={this.state.board[y][x]} key={coord} flipCellsAroundMe={()=>this.flipCellsAround(coord)}/> )
       }
       tblBoard.push(<tr>{row}</tr>)
     }
