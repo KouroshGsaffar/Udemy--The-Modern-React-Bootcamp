2423075d2e8d9360491a55a847b47d5e24173b6d
Kourosh Gharouni Saffar <kooroshgharooni74@gmail.com>
2021-03-03 18:41:46 -0500

    Dad-jokes

--- 18. Lifecycle Exercise/card-dealer/src/Card.js
+++ 18. Lifecycle Exercise/card-dealer/src/Card.js
@@ -4,10 +4,10 @@ import './Card.css';
 export default class Card extends Component {
     constructor(props){
         super(props)
-    let angle = Math.random() * 90 - 45;
-    let xPos = Math.random() * 40 - 20;
-    let yPos = Math.random() * 40 - 20;
-    this._transform = `translate(${xPos}px, ${yPos}px) rotate(${angle}deg)`;
+        let angle = Math.random() * 90 - 45;
+        let xPos = Math.random() * 40 - 20;
+        let yPos = Math.random() * 40 - 20;
+        this._transform = `translate(${xPos}px, ${yPos}px) rotate(${angle}deg)`;
     
     }
     render() {
