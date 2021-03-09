afecc2e271c1f18e62f7aa137a9a80dd9068bffd
Kourosh Gharouni Saffar <kooroshgharooni74@gmail.com>
2021-03-09 16:35:25 -0500

    Fix Palette Route

--- 24. Massive Color Project/color-project/src/App.js
+++ 24. Massive Color Project/color-project/src/App.js
@@ -1,3 +1,5 @@
+import React, { Component } from 'react'
+
 import './App.css';
 import Palette from './Palette';
 import { Route, Switch } from 'react-router-dom'
@@ -5,16 +7,24 @@ import seedColors from './seedColors';
 import { generatePalette } from "./colorHelpers";
 
 
-function App() {
-  return (
-    <Switch>
-      <Route exact path='/' render={() => <h1>Pallete List Goes here!</h1>} />
-      <Route exact path='/palette/:id' render={() => <h1>Pallete individual</h1>} />
-      {/* <div>
-          <Palette palette={generatePalette(seedColors[4])} />
-        </div>  */}
-    </Switch>
-  );
+class App extends Component {
+  findPallete(id) {
+    return seedColors.find(function (palette) {
+      return palette.id === id;
+    })
+  }
+  render() {
+    return (
+      <Switch>
+        <Route exact path='/' render={() => <h1>Pallete List Goes here!</h1>} />
+        <Route exact path='/palette/:id' render={(routeProps) => <Palette
+          palette={generatePalette(this.findPallete(routeProps.match.params.id))} />} />
+        {/* <div>
+            <Palette palette={generatePalette(seedColors[4])} />
+          </div>  */}
+      </Switch>
+    );
+  }
 }
 
 export default App;
