e4a88362a4157dea06e6e0820a6dbfd2f13bf9d7
Kourosh Gharouni Saffar <kooroshgharooni74@gmail.com>
2021-03-09 15:56:10 -0500

    Add react router

--- 24. Massive Color Project/color-project/src/App.js
+++ 24. Massive Color Project/color-project/src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
 import Palette from './Palette';
+import { Route, Switch } from 'react-router-dom'
 import seedColors from './seedColors';
 import { generatePalette } from "./colorHelpers";
 
 
 function App() {
   return (
-    <div>
-      <Palette palette={generatePalette(seedColors[4])} />
-    </div>
+    <Switch>
+      <Route exact path='/' render={() => <h1>Pallete List Goes here!</h1>} />
+      <Route exact path='/palette/:id' render={() => <h1>Pallete individual</h1>} />
+      {/* <div>
+          <Palette palette={generatePalette(seedColors[4])} />
+        </div>  */}
+    </Switch>
   );
 }
 
