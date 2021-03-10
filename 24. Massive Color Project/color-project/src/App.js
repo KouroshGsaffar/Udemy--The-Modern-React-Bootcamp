d349c0aa6e12fa049235fb362e6eb7b40eef8734
Kourosh Gharouni Saffar <kooroshgharooni74@gmail.com>
2021-03-10 16:56:13 -0500

    Add SingleColorPalette and Link

--- 24. Massive Color Project/color-project/src/App.js
+++ 24. Massive Color Project/color-project/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-
 import './App.css';
 import Palette from './Palette';
 import { Route, Switch } from 'react-router-dom'
 import seedColors from './seedColors';
 import { generatePalette } from "./colorHelpers";
 import PaletteList from './PaletteList';
+import SingleColorPalette from './SingleColorPalette';
 
 
 class App extends Component {
@@ -20,7 +20,7 @@ class App extends Component {
         <Route exact path='/' render={(routeProps) => <PaletteList palettes={seedColors} {...routeProps} />} />
         <Route exact path='/palette/:id' render={(routeProps) => <Palette
           palette={generatePalette(this.findPallete(routeProps.match.params.id))} />} />
-        <Route exact path='/palette/:paletteId/:colorId' render={() => <h1>Single Color Page!</h1>} />
+        <Route exact path='/palette/:paletteId/:colorId' render={() => <SingleColorPalette />} />
       </Switch>
     );
   }
--- 24. Massive Color Project/color-project/src/ColorBox.js
+++ 24. Massive Color Project/color-project/src/ColorBox.js
@@ -18,7 +18,7 @@ class ColorBox extends Component {
         })
     }
     render() {
-        const { background, name } = this.props;
+        const { background, name, paletteId, id, moreUrl } = this.props;
         const { copied } = this.state;
         return (
             <CopyToClipboard text={background} onCopy={this.changeCopyState}>
@@ -34,7 +34,7 @@ class ColorBox extends Component {
                         </div>
                         <button className='copy-button'>Copy</button>
                     </div>
-                    <Link to='/' onClick={(e) => e.stopPropagation()}>
+                    <Link to={moreUrl} onClick={(e) => e.stopPropagation()}>
                         <span className='see-more'>More</span>
                     </Link>
                 </div>
