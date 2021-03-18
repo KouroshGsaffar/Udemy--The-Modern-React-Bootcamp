f256d54c078ac11ce3be27c3722e5483361ed336
Kourosh Gharouni Saffar <kooroshgharooni74@gmail.com>
2021-03-18 19:57:58 -0400

    Custom Hook reducer and editing  LocalStorage

--- 26. React Hooks Project/todos-hooks/src/TodoForm.js
+++ 26. React Hooks Project/todos-hooks/src/TodoForm.js
@@ -5,7 +5,7 @@ import { DispatchContext } from './context/todos.context';
 
 export default function TodoForm() {
     const [value, handleChange, reset] = useInputState("");
-    const { dispatch } = useContext(DispatchContext);
+    const dispatch = useContext(DispatchContext);
     return (
         <Paper style={{ margin: "1rem 0", padding: "0 1rem" }}>
             <form
--- 26. React Hooks Project/todos-hooks/src/context/todos.context.js
+++ 26. React Hooks Project/todos-hooks/src/context/todos.context.js
@@ -1,4 +1,5 @@
-import React, { useReducer, createContext } from "react";
+import React, { createContext } from "react";
+import useLocalStorageReducer from "../hooks/useLocalStorageReducer";
 import useTodoState from "../hooks/useTodoState";
 import todoReducer from "../reducers/todo.reducer";
 
@@ -10,7 +11,7 @@ export const TodosContext = createContext();
 export const DispatchContext = createContext();
 
 export function TodosProvider(props) {
-    const [todos, dispatch] = useReducer(todoReducer, defaultTodos);
+    const [todos, dispatch] = useLocalStorageReducer('todos', defaultTodos, todoReducer);
     return (
         <TodosContext.Provider value={todos}>
             <DispatchContext.Provider value={dispatch}>
