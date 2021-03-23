170e909fd6ee2deab0137e68f31794d0c817103f
Kourosh Gharouni Saffar <kooroshgharooni74@gmail.com>
2021-03-23 00:17:43 -0400

    working with Nextjs

---
+++ 29. Nextjs/first-next/pages/contact.js
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar";
+
+const ContactPage = () => (
+    <div>
+        <Navbar />
+        <h1>Contact Page</h1>
+        <p>Do not contact us ever</p>
+    </div>
+)
+
+export default ContactPage;
---
+++ 29. Nextjs/first-next/pages/index.js
@@ -0,0 +1,9 @@
+import Navbar from "../components/Navbar";
+const Index = () => (
+    <div>
+        <Navbar />
+        <h1>Our Index Page!!!</h1>
+    </div>
+)
+
+export default Index;
