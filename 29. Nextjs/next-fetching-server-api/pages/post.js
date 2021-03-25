4df1238d1a35398c928e92fa07104b5591b0df70
Kourosh Gharouni Saffar <kooroshgharooni74@gmail.com>
2021-03-24 22:28:26 -0400

    Fetching Comments

--- 29. Nextjs/next-fetching-server-api/pages/post.js
+++ 29. Nextjs/next-fetching-server-api/pages/post.js
@@ -1,8 +1,23 @@
-const Post = ({ id }) => (
-    <h1>You are looking at post #{id}</h1>
+import axios from 'axios'
+
+const Post = ({ id, comments }) => (
+    <>
+        <h1>Comments for Post#{id}</h1>
+        {comments.map(comment => (
+            <Comment {...comment} key={comment.id} />
+        ))}
+    </>
+)
+const Comment = ({ email, body }) => (
+    <div>
+        <h5>{email}</h5>
+        <p>{body}</p>
+    </div>
 )
 Post.getInitialProps = async ({ query }) => {
-    return query
+    const res = await axios.get(`https://jsonplaceholder.typicode.com/comments?postId=${query.id}`)
+    const { data } = res;
+    return { ...query, comments: data }
 }
 
 export default Post
