fc6d69b91f08b76801a893a89c8e7ebcf07cecf3
Kourosh Gharouni Saffar <kooroshgharooni74@gmail.com>
2021-05-11 23:21:14 -0400

    Implementing ApprovalCard and Showing custom children

--- Exercise/components/src/App.js
+++ Exercise/components/src/App.js
@@ -1,11 +1,22 @@
 import CommentDetail from './CommentDetail'
 import faker from 'faker'
+import ApprovalCard from './ApprovalCard';
 
 function App() {
   return (
     <div className="ui container comments">
-    <CommentDetail name='Sam' comment='Nice post!' date='Today 6:00 PM' avatar={faker.image.avatar()}/>
-    <CommentDetail name='George' comment='Awwwww!' date='Yesterday 2:00 PM' avatar={faker.image.avatar()} />
+      <ApprovalCard>
+       <div>
+         <h4> Warning</h4>
+         Are you going to continue?
+       </div>
+      </ApprovalCard>
+      <ApprovalCard>
+       <CommentDetail name='Sam' comment='Nice post!' date='Today 6:00 PM' avatar={faker.image.avatar()}/>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail name='George' comment='Awwwww!' date='Yesterday 2:00 PM' avatar={faker.image.avatar()} />
+      </ApprovalCard>
     </div>
   );
 }
