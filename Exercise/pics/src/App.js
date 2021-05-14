62bed692c866ce83d02038958ba43a033df30323
Kourosh Gharouni Saffar <kooroshgharooni74@gmail.com>
2021-05-14 19:28:20 -0400

    create a new project- adding searchbar component

---
+++ Exercise/pics/src/App.js
@@ -0,0 +1,11 @@
+import SearchBar from './components/SearchBar';
+
+function App() {
+  return (
+    <div className='ui container' style={{ marginTop: '10px' }}>
+      <SearchBar />
+    </div>
+  );
+}
+
+export default App;
