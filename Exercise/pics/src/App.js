fd4f78b9811de49189d4dacf019b4c1244c3cbad
Kourosh Gharouni Saffar <kooroshgharooni74@gmail.com>
2021-05-15 14:25:01 -0400

    Fetching data in App component using axios

--- Exercise/pics/src/App.js
+++ Exercise/pics/src/App.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 import SearchBar from './components/SearchBar';
 
 export default class App extends Component {
-  async onSearchSubmit(term) {
+  state = { images: [] }
+  onSearchSubmit = async (term) => {
     const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: { query: term },
       headers: {
@@ -11,12 +12,14 @@ export default class App extends Component {
           'Client-ID WLL5TVVTRz_0UpJDS8nPCdu6uVDxhmiTBJlWtAXcs3Y',
       },
     })
-    console.log(response.data.results)
+    this.setState({ images: response.data.results })
+    // console.log(response.data.results)
   }
   render() {
     return (
       <div className='ui container' style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     )
   }
