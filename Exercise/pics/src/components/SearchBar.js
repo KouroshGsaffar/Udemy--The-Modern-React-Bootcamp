52e8c5ca094f8b42a45b795a8da68f070954c42a
Kourosh Gharouni Saffar <kooroshgharooni74@gmail.com>
2021-05-14 20:01:46 -0400

    add onInputChange funtionality

--- Exercise/pics/src/components/SearchBar.js
+++ Exercise/pics/src/components/SearchBar.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
 
 export default class SearchBar extends Component {
+    state = { term: '' }
+    onInputChange = (event) => {
+        // event.preventDefault()
+        this.setState({ term: event.target.value })
+    }
     render() {
         return (
             <div className='ui segment'>
                 <form className='ui form'>
                     <div className='field'>
                         <label>Image Search</label>
-                        <input type='text' />
+                        <input type='text' value={this.state.term} onChange={this.onInputChange} />
                     </div>
 
                 </form>
