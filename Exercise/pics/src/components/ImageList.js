8b570233c4823f26324f6d3f322c281a4cc44148
Kourosh Gharouni Saffar <kooroshgharooni74@gmail.com>
2021-05-16 13:29:22 -0400

    implementing key and alt in lists

--- Exercise/pics/src/components/ImageList.js
+++ Exercise/pics/src/components/ImageList.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
 export default function ImageList(props) {
-    const images = props.images.map(image => {
-        return <img src={image.urls.regular} />
+    const images = props.images.map(({ description, id, urls }) => {
+        return <img alt={description} key={id} src={urls.regular} />
     })
     return (
         <div>
