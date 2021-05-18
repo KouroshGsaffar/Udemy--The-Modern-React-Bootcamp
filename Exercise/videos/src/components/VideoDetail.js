274fe2e4e7a438ec109749c6f7a52d42c980fd75
Kourosh Gharouni Saffar <kooroshgharooni74@gmail.com>
2021-05-18 19:23:07 -0400

    add semantic ui grid style and fixing a few warnings

--- Exercise/videos/src/components/VideoDetail.js
+++ Exercise/videos/src/components/VideoDetail.js
@@ -8,7 +8,7 @@ export default function VideoDetail({ video }) {
     return (
         <div>
             <div className='ui embed'>
-                <iframe src={videoSrc} />
+                <iframe title='video player' src={videoSrc} />
             </div>
             <div className='ui segment'>
                 <h4 className='ui header'>{video.snippet.title}</h4>
