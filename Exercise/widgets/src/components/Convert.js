21bbf0fe20c5ae718dfabc4e12660de5b08cdf1f
Kourosh Gharouni Saffar <kooroshgharooni74@gmail.com>
2021-05-26 23:21:28 -0400

    Displaying translated Text

--- Exercise/widgets/src/components/Convert.js
+++ Exercise/widgets/src/components/Convert.js
@@ -2,23 +2,25 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 export default function Convert({ language, text }) {
+    const [translated, setTranslated] = useState('')
     useEffect(() => {
-        // const api=async()=>{
+        const doTranslation = async () => {
+            const { data } = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
+                params: {
+                    q: text,
+                    target: language.value,
+                    key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
+                }
+            })
+            setTranslated(data.data.translations[0].translatedText)
+        }
 
-        // }
-        axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
-            params: {
-                q: text,
-                target: language.value,
-                key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
-            }
-        })
-        // console.log('new language or text');
+        doTranslation()
 
     }, [language, text])
     return (
         <div>
-
+            <h1 className='ui header'>{translated}</h1>
         </div>
     )
 }
