3dfa24419388bb1c1350328d118dbc8be39ffd6e
Kourosh Gharouni Saffar <kooroshgharooni74@gmail.com>
2021-05-30 21:35:25 -0400

    Conditional Rendering

--- Exercise/songs/src/App.js
+++ Exercise/songs/src/App.js
@@ -1,4 +1,4 @@
-import { selectSong } from "./actions";
+import SongDetail from "./components/SongDetail";
 import SongList from "./components/SongList";
 
 
@@ -8,6 +8,9 @@ function App() {
       <div className='ui row'>
         <div className='column eight wide'>
           <SongList />
+        </div>
+        <div className='column eight wide'>
+          <SongDetail />
         </div>
       </div>
     </div>
